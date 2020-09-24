f2f77329de496ab2cbeffc2f335d38bd4ad46419
Julius Oeftiger <j.oeftiger@protonmail.com>
2020-09-24 17:52:19 +0200

    main rust_v: Added quick_bench()

--- src/bin/rust_v.rs
+++ src/bin/rust_v.rs
@@ -1,6 +1,10 @@
 extern crate clap;
 
 use clap::{App, Arg};
+use std::time::Instant;
+use rust_v::geometry::sphere::Sphere;
+use ultraviolet::Vec3;
+use rust_v::geometry::{Ray, Intersectable};
 
 const INPUT: &str = "input_file";
 const OUTPUT: &str = "output_file";
@@ -9,6 +13,33 @@ const VERBOSE: &str = "VERBOSE";
 fn main() {
     // let app = init_help();
     // let _matches = app.get_matches();
+
+   quick_bench();
+}
+
+fn quick_bench() {
+   const SECONDS: u64 = 300;
+
+   let sphere = Sphere::new(Vec3::zero(), 1.0);
+   let ray = Ray::new(-Vec3::unit_x() * 2.0, Vec3::unit_x());
+
+   let now = Instant::now();
+
+   println!("Warming up for {} s...", 5);
+   while now.elapsed().as_secs() < 5 {
+      let _hit = sphere.intersects(&ray);
+   }
+
+   println!("Benchmarking for {} s...", SECONDS);;
+   let mut hits = 0;
+   let now = Instant::now();
+
+   while now.elapsed().as_secs() < SECONDS {
+      let _hit = sphere.intersects(&ray);
+      hits += 1;
+   }
+
+   println!("{} casts/s", hits / SECONDS);
 }
 
 fn init_help<'a, 'b>() -> App<'a, 'b> {
