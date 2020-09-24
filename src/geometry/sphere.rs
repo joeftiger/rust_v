fe07a4bfa82cc72fd6b970e07dcee59dc12dd03d
Julius Oeftiger <j.oeftiger@protonmail.com>
2020-09-24 17:49:27 +0200

    Sphere: optimize ray intersection

--- src/geometry/sphere.rs
+++ src/geometry/sphere.rs
@@ -6,6 +6,15 @@ pub struct Sphere {
     radius: f32,
 }
 
+impl Sphere {
+    pub fn new(center: Vec3, radius: f32) -> Self {
+        Sphere {
+            center,
+            radius,
+        }
+    }
+}
+
 impl Boxable for Sphere {
     fn bounding_box(&self) -> Option<Aabb> {
         let offset = Vec3::one() * self.radius;
@@ -19,11 +28,10 @@ impl Intersectable<&Ray> for Sphere {
         let oc = ray.origin - self.center;
 
         let a = ray.direction.mag_sq();
-        let b = 2.0 * oc.dot(ray.direction);
+        let b = oc.dot(ray.direction);
         let c = oc.mag_sq() - self.radius * self.radius;
-        let discriminant = b * b - 4.0 * a * c;
 
-        discriminant > 0.0
+        b * b > a * c
     }
 }
 
