cd9c99977a4ef118159a3e10f885191ffca9b706
Julius Oeftiger <j.oeftiger@protonmail.com>
2020-10-04 16:37:29 +0200

    add distance, project api to plane

--- src/geometry/plane.rs
+++ src/geometry/plane.rs
@@ -20,14 +20,39 @@ impl Plane {
         }
     }
 
-    pub fn from(position: Vec3, normal: Vec3) -> Self {
+    /// Calculates the projected position of the given point to the plane.
+    #[inline]
+    #[must_use]
+    pub fn project(&self, point: Vec3) -> Vec3 {
+        point - (self.normal * self.distance(point))
+    }
+
+    /// Calculates the distance of the given point to the plane.
+    #[inline]
+    #[must_use]
+    pub fn distance(&self, point: Vec3) -> f32 {
+        let v = point - (self.normal * self.d);
+        let angle = self.normal.angle_to(v);
+
+        angle.cos() * v.mag()
+    }
+}
+
+impl From<(Vec3, Vec3)> for Plane {
+    fn from(pos_normal: (Vec3, Vec3)) -> Self {
+        let position = pos_normal.0;
+        let normal = pos_normal.1.normalized();
+
         let angle = position.angle_to(normal);
         let d = angle.cos() * position.mag();
 
-        Self {
-            d,
-            normal: normal.normalized(),
-        }
+        Self { d, normal }
+    }
+}
+
+impl Boxable for Plane {
+    fn bounding_box(&self) -> Option<Aabb> {
+        None
     }
 }
 
@@ -51,11 +76,5 @@ impl Intersectable<Ray> for Plane {
         }
 
         Some(Intersection::new(position, normal, t))
-    }
-}
-
-impl Boxable for Plane {
-    fn bounding_box(&self) -> Option<Aabb> {
-        None
     }
 }
