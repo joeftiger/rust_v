027f5a75472a141ed3609498a9ea04afeb575081
Julius Oeftiger <j.oeftiger@protonmail.com>
2020-10-04 17:57:09 +0200

    Add Cylinder

---
+++ src/geometry/cylinder.rs
@@ -0,0 +1,68 @@
+use ultraviolet::Vec3;
+use crate::geometry::{Boxable, Intersectable, Ray, Intersection};
+use crate::geometry::aabb::Aabb;
+use crate::math;
+
+// A geometrical cylinder.
+pub struct Cylinder {
+    pub center: Vec3,
+    pub axis: Vec3,
+    pub radius: f32,
+    pub height: f32,
+}
+
+impl Cylinder {
+    pub fn new(center: Vec3, axis: Vec3, radius: f32, height: f32) -> Self {
+        Self { center, axis, radius, height }
+    }
+}
+
+impl Boxable for Cylinder {
+    fn bounding_box(&self) -> Option<Aabb> {
+        let scale = self.height / 2.0 + f32::sqrt(2.0 * self.radius * self.radius);
+        let offset = Vec3::one() * scale;
+
+        Some(Aabb::new(self.center - offset, self.center + offset))
+    }
+}
+
+impl Intersectable<Ray> for Cylinder {
+    fn intersects(&self, ray: Ray) -> Option<Intersection> {
+        let dir = ray.direction;
+        let oc = ray.origin - self.center;
+
+        let dir_parallel = self.axis.dot(dir);
+        let oc_parallel = self.axis.dot(oc);
+
+        let a = dir.dot(dir) - dir_parallel * dir_parallel;
+        let b = 2.0 * (dir.dot(oc) - dir_parallel * oc_parallel);
+        let c = oc.dot(oc) - oc_parallel * oc_parallel - self.radius * self.radius;
+
+        // Find the closest valid solution
+        // (in front of the viewer and within the cylinder's height).
+        let mut t = f32::NEG_INFINITY;
+        for sol in math::solve_quadratic(a, b, c) {
+            if sol <= 0.0 {
+                continue;
+            }
+            let z = self.axis.dot(ray.at(sol) - self.center);
+            if 2.0 * z.abs() < self.height {
+                t = t.min(sol);
+            }
+        }
+        if t == f32::NEG_INFINITY {
+            return None;
+        }
+
+        let position = ray.at(t);
+        let mut normal = self.axis.dot((position - self.center) / self.radius) * self.axis;
+
+        // Choose the normal's orientation to be opposite the ray's
+        // (in case the ray intersects the inside surface)
+        if normal.dot(dir) > 0.0 {
+            normal *= -1.0;
+        }
+
+        return Some(Intersection::new(position, normal, t));
+    }
+}
