72e5ae0c8c293881e2b5c15ace63409dc26a0762
Julius Oeftiger <j.oeftiger@protonmail.com>
2020-10-06 20:01:51 +0200

    Refactor Ray into a new file, create trait 'Ray' and objects 'NormalRay' and 'ShadowRay'

--- src/scene.rs
+++ src/scene.rs
@@ -1,7 +1,8 @@
-use crate::geometry::{Boxable, Intersectable, Ray};
+use crate::geometry::{Boxable, Intersectable};
 use serde::{Serialize, Deserialize};
+use crate::geometry::ray::Ray;
 
 
-pub trait SceneObject<'a>: Boxable + Intersectable<Ray> + Serialize + Deserialize<'a> {}
+pub trait SceneObject<'a, T: Ray>: Boxable + Intersectable<T> + Serialize + Deserialize<'a> {}
 
 pub struct Scene {}
