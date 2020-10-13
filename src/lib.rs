404fa189391dd9e679f987fb15b7ea659d7896eb
Julius Oeftiger <j.oeftiger@protonmail.com>
2020-10-14 01:16:13 +0200

    Remove scene.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,9 @@
 // to enable f32 sorting by comparison
 #![feature(total_cmp)]
-#![feature(const_generics)]
 
 pub mod geometry;
 pub mod physics;
-pub mod scene;
 pub mod structure;
 pub mod util;
 pub mod math;
+pub mod render;
--- src/scene.rs
+++
@@ -1,8 +0,0 @@
-use crate::geometry::{Boxable, Intersectable};
-use serde::{Serialize, Deserialize};
-use crate::geometry::ray::Ray;
-
-
-pub trait SceneObject<'a, T: Ray>: Boxable + Intersectable<T> + Serialize + Deserialize<'a> {}
-
-pub struct Scene {}
