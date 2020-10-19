56ee763f6550ebdeebe4a6b8e2641dae566a3fa5
Julius Oeftiger <j.oeftiger@protonmail.com>
2020-10-19 13:18:56 +0200

    add API for unit aabbs

--- src/geometry/aabb.rs
+++ src/geometry/aabb.rs
@@ -1,10 +1,58 @@
 use serde::{Deserialize, Serialize};
 use ultraviolet::Vec3;
 
-use crate::geometry::{Intersectable, Intersection};
 use crate::geometry::plane::Plane;
 use crate::geometry::ray::Ray;
+use crate::geometry::{Intersectable, Intersection};
 use crate::Float;
+
+/// Creates a unit Aabb with size `(-Vec3::one(), Vec3::one())`.
+pub fn unit_aabb() -> Aabb {
+    Aabb::new(-Vec3::one(), Vec3::one())
+}
+
+/// # Summary
+/// Optimized ray intersection against a unit Aabb
+///
+pub fn intersects_unit_aabb(ray: &Ray) -> Option<Intersection> {
+    let t1 = (-Vec3::one() - ray.origin) / ray.direction;
+    let t2 = (Vec3::one() - ray.origin) / ray.direction;
+
+    let t_min_vec = t1.min_by_component(t2);
+    let t_max_vec = t1.max_by_component(t2);
+
+    let t_min = Float::max(
+        t_min_vec.z as Float,
+        Float::max(t_min_vec.y as Float, t_min_vec.x as Float),
+    );
+    let t_max = Float::min(
+        t_max_vec.z as Float,
+        Float::min(t_max_vec.y as Float, t_max_vec.x as Float),
+    );
+
+    if t_max < 0.0 || t_max < t_min {
+        return None;
+    }
+
+    let position = ray.at(t_min);
+    let normal: Vec3;
+
+    if (position.x + 1.0) as Float <= Float::EPSILON {
+        normal = -Vec3::unit_x();
+    } else if (position.x - 1.0) as Float <= Float::EPSILON {
+        normal = Vec3::unit_x();
+    } else if (position.y + 1.0) as Float <= Float::EPSILON {
+        normal = -Vec3::unit_y();
+    } else if (position.y - 1.0) as Float <= Float::EPSILON {
+        normal = Vec3::unit_y();
+    } else if (position.z + 1.0) as Float <= Float::EPSILON {
+        normal = -Vec3::unit_z();
+    } else {
+        normal = Vec3::unit_z();
+    }
+
+    Some(Intersection::new(position, normal, t_min))
+}
 
 /// An geometrical axis-aligned bounding box.
 #[derive(Clone, Copy, Debug, Default, Deserialize, Serialize)]
@@ -62,7 +110,8 @@ impl Aabb {
     #[must_use]
     pub fn volume(&self) -> Float {
         // max is guaranteed to be greater-or-equal to min.
-        ((self.max.x - self.min.x) * (self.max.y - self.min.y) * (self.max.z - self.min.z).abs()) as Float
+        ((self.max.x - self.min.x) * (self.max.y - self.min.y) * (self.max.z - self.min.z).abs())
+            as Float
     }
 
     /// Calculates the size.
@@ -121,8 +170,14 @@ impl Intersectable<Ray> for Aabb {
         let t_min_vec = t1.min_by_component(t2);
         let t_max_vec = t1.max_by_component(t2);
 
-        let t_min = Float::max(t_min_vec.z as Float, Float::max(t_min_vec.y as Float, t_min_vec.x as Float));
-        let t_max = Float::min(t_max_vec.z as Float, Float::min(t_max_vec.y as Float, t_max_vec.x as Float));
+        let t_min = Float::max(
+            t_min_vec.z as Float,
+            Float::max(t_min_vec.y as Float, t_min_vec.x as Float),
+        );
+        let t_max = Float::min(
+            t_max_vec.z as Float,
+            Float::min(t_max_vec.y as Float, t_max_vec.x as Float),
+        );
 
         if t_max < 0.0 || t_max < t_min {
             return None;
@@ -144,7 +199,7 @@ impl Intersectable<Ray> for Aabb {
         } else {
             normal = Vec3::unit_z();
         }
-        
+
         Some(Intersection::new(position, normal, t_min))
     }
 }
