2ef21d4d57c6aa34d9edbdbe6e72ebc81bda2028
Julius Oeftiger <j.oeftiger@protonmail.com>
2020-10-20 15:09:38 +0200

    Remove feature flags

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,3 @@
-// to enable f32 sorting by comparison
-#![feature(total_cmp)]
-// to enable Spectrum<const SAMPLES: usize>
-#![feature(const_generics)]
-
 pub mod geometry;
 pub mod math;
 pub mod physics;
