c1518863c4cb5f231f673b05be4fc946ec742f12
Julius Oeftiger <j.oeftiger@protonmail.com>
2020-10-21 21:00:32 +0200

    Add check_intersection() api to simplify life

--- src/acceleration_structure/mod.rs
+++ src/acceleration_structure/mod.rs
@@ -1,15 +1,26 @@
 use ultraviolet::Vec3;
 
 use crate::geometry::aabb::Aabb;
-use crate::geometry::{Boxable, Intersection};
+use crate::geometry::{Boxable, Intersection, Intersectable};
 use crate::render::Scene;
 use crate::geometry::ray::Ray;
 use crate::render::objects::SceneObject;
 
 pub mod kd_tree;
 pub mod uniform_spatial_partition;
+pub mod no_acceleration;
 
-fn global_bounding_box(objects: &Vec<Box<dyn SceneObject>>) -> Option<Aabb> {
+fn check_intersection(ray: &Ray, object: &Box<dyn SceneObject>) -> Option<Intersection> {
+    if let Some(aabb) = object.bounding_box() {
+        if aabb.intersects(ray).is_some() {
+            return object.intersects(ray);
+        }
+    }
+
+    None
+}
+
+fn global_bounding_box(objects: &[Box<dyn SceneObject>]) -> Option<Aabb> {
     if objects.is_empty() {
         return None;
     }
@@ -35,7 +46,7 @@ fn global_bounding_box(objects: &Vec<Box<dyn SceneObject>>) -> Option<Aabb> {
     Some(aabb)
 }
 
-fn average_cell_size(objects: &Vec<Box<dyn SceneObject>>) -> Option<Vec3> {
+fn average_cell_size(objects: &[Box<dyn SceneObject>]) -> Option<Vec3> {
     if objects.is_empty() {
         return None;
     }
