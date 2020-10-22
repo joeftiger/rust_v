c0f9f90484563af2cc76dce3bbf94fda3277424c
Julius Oeftiger <j.oeftiger@protonmail.com>
2020-10-22 16:53:53 +0200

    fix critical bug

--- src/math.rs
+++ src/math.rs
@@ -1,19 +1,19 @@
 use crate::util::floats;
 
 pub fn solve_quadratic(a: f32, b: f32, c: f32) -> Vec<f32> {
-    if floats::approx_zero(a) {
-        if floats::approx_zero(b) {
+    if floats::lt_epsilon(a) {
+        if floats::lt_epsilon(b) {
             return vec![];
         }
         return vec![-c / b];
     }
 
-    let discrimant = b * b - 4.0 * a * c;
-    if discrimant < 0.0 {
+    let discriminant = b * b - 4.0 * a * c;
+    if discriminant < 0.0 {
         return vec![];
     }
 
-    let a_x1 = -0.5 * (b * f32::copysign(discrimant.sqrt(), b));
+    let a_x1 = -0.5 * (b + f32::copysign(discriminant.sqrt(), b));
 
     vec![a_x1 / a, c / a_x1]
 }
