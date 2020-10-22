d2a212aba4a0587ea8e0f61d690707a7faa40063
Julius Oeftiger <j.oeftiger@protonmail.com>
2020-10-23 01:24:51 +0200

    Discern between live rendering and saving to file immediately

--- src/bin/rust_v.rs
+++ src/bin/rust_v.rs
@@ -1,38 +1,64 @@
-use rust_v::geometry::sphere::Sphere;
+use clap::{App, Arg};
 use ultraviolet::Vec3;
-use rust_v::render::camera::Camera;
-use rust_v::render::scene::Scene;
-use rust_v::render::renderer::{RgbRenderer, Renderer};
-use rust_v::render::scene_objects::SceneObject;
+
 use rust_v::color::srgb::Srgb;
+use rust_v::geometry::sphere::Sphere;
+use rust_v::render::camera::Camera;
+use rust_v::render::renderer::debug::NormalRenderer;
+use rust_v::render::renderer::Renderer;
+use rust_v::render::scene::Scene;
+use rust_v::render::scene_objects::SceneObject;
 use rust_v::render::window::RenderWindow;
 
+const LIVE_WINDOW: &str = "LIVE_WINDOW";
+
 fn main() {
-    let objects = vec![
-        SceneObject::new(Sphere::default(), Srgb::new(1.0, 0.0, 0.0)),
-    ];
+    let app = init_help();
+    let matches = app.get_matches();
 
+    let mut renderer = create_renderer();
+
+    if matches.is_present(LIVE_WINDOW) {
+        let mut window =
+            RenderWindow::new("Rust-V".to_string(), renderer).expect("Can't create window");
+
+        window.start_rendering();
+    } else {
+        renderer.render_all();
+
+        let image = renderer.get_image();
+        image.save("./rendering.png").unwrap();
+    }
+}
+
+fn init_help<'a, 'b>() -> App<'a, 'b> {
+    App::new("Rust-V")
+        .version("0.0.1")
+        .author("Julius Oeftiger")
+        .about("A rust ray tracer supporting rgb and spectral ray tracing")
+        .arg(
+            Arg::with_name(LIVE_WINDOW)
+                .short("l")
+                .long("live")
+                .help("Use a live window to show rendering progressively. The window allows saving with [Ctrl + S]")
+                .required(false)
+        )
+}
+
+fn create_renderer() -> impl Renderer {
+    let objects = vec![SceneObject::new(
+        Sphere::default(),
+        Srgb::new(1.0, 0.0, 0.0),
+    )];
     let scene = Scene::new(objects);
-
     let camera = Camera::new(
         -2.0 * Vec3::unit_x(),
         Vec3::zero(),
         Vec3::unit_z(),
         120.0,
         1280,
-        720
+        720,
     );
 
-    let mut renderer = RgbRenderer::new(scene, camera);
-
-    while !renderer.is_done() {
-        renderer.render_pass();
-    }
-
-    let image = renderer.get_image();
-    image.save("./rendering.png").unwrap();
-
-    // let mut window = RenderWindow::new("Rust-V".to_string(), renderer).unwrap();
-    //
-    // window.start_rendering();
+    NormalRenderer::new(scene, camera)
 }
