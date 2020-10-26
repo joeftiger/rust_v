5db5f37052bfc9e64f711ee828a948c908544180
Julius Oeftiger <j.oeftiger@protonmail.com>
2020-10-26 20:42:32 +0100

    Light structure

--- src/render/light.rs
+++ src/render/light.rs
@@ -2,49 +2,53 @@ use ultraviolet::Vec3;
 
 use crate::color::Srgb;
 use crate::geometry::aabb::Aabb;
-use crate::geometry::cube::Cube;
-use crate::geometry::intersection::Intersection;
-use crate::geometry::point::Point;
 use crate::geometry::ray::Ray;
-use crate::geometry::sphere::Sphere;
-use crate::geometry::Geometry;
+use crate::geometry::{Geometry, Hit, GeometryInfo};
+use std::ops::Mul;
 
-macro_rules! lights {
-    ($($name:ident => $vec:ident, $ray:ident, $shape:ident, $rad:ident), +) => {
-        $(
-            pub struct $name {
-                pub position: $vec,
-                pub shape: $shape,
-                pub radiance: $rad,
-            }
-
-            impl $name {
-                pub fn ray_to(&self, position: $vec) -> $ray {
-                    let mut ray = $ray::new_simple(self.position, position);
-                    ray.set_radiance(self.radiance.clone());
-                    ray
-                }
-
-                pub fn distance(&self, position: $vec) -> f32 {
-                    (position - self.position).mag()
-                }
-            }
-
-            impl Geometry<$ray, Intersection> for $name {
-                fn bounding_box(&self) -> Aabb {
-                    self.shape.bounding_box()
-                }
-
-                fn intersect(&self, ray: &Ray) -> Option<Intersection> {
-                    self.shape.intersect(ray)
-                }
-            }
-        )+
-    };
+pub struct Light {
+    pub point: Vec3,
+    shape: Box<dyn Geometry>,
+    pub color: Srgb,
+    pub intensity: f32,
 }
 
-lights!(
-    PointLight => Vec3, Ray, Point, Srgb,
-    SphericalLight => Vec3, Ray, Sphere, Srgb,
-    CubicLight => Vec3, Ray, Cube, Srgb
-);
+impl Light {
+    pub fn new(point: Vec3, shape: Box<dyn Geometry>, color: Srgb, intensity: f32) -> Self {
+        Self { point, shape, color, intensity }
+    }
+
+    pub fn ray_to(&self, point: Vec3) -> Ray {
+        Ray::new_simple(self.point, point - self.point)
+    }
+
+    pub fn distance(&self, point: Vec3) -> f32 {
+        (self.point - point).mag()
+    }
+
+    pub fn intensity_at(&self, point: Vec3) -> f32 {
+        let distance = self.distance(point);
+
+        self.intensity / (distance * distance)
+    }
+
+    pub fn color_at(&self, point: Vec3) -> Srgb {
+        let intensity = self.intensity_at(point);
+
+        self.color.mul(intensity)
+    }
+}
+
+impl Geometry for Light {
+    fn bounding_box(&self) -> Aabb {
+        self.shape.bounding_box()
+    }
+
+    fn intersect(&self, ray: &Ray) -> Option<f32> {
+        self.shape.intersect(ray)
+    }
+
+    fn get_info(&self, hit: Hit) -> GeometryInfo {
+        self.shape.get_info(hit)
+    }
+}
