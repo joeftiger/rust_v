bb10ba572ca20ccdc168d8709ebfce0735b1de48
Julius Oeftiger <j.oeftiger@protonmail.com>
2020-10-27 20:13:25 +0100

    Use Option of two floats instead of vec

--- src/math.rs
+++ src/math.rs
@@ -1,24 +1,26 @@
 use crate::floats;
 
-pub fn solve_quadratic(a: f32, b: f32, c: f32) -> Vec<f32> {
+pub fn solve_quadratic(a: f32, b: f32, c: f32) -> Option<(f32, f32)> {
     debug_assert!(!a.is_nan());
     debug_assert!(!b.is_nan());
     debug_assert!(!c.is_nan());
 
     if floats::lt_epsilon(a) {
         if floats::lt_epsilon(b) {
-            return vec![];
+            return None;
         }
 
-        return vec![-c / b];
+        let sol = -c / b;
+
+        return Some((sol, sol));
     }
 
     let discriminant = b * b - 4.0 * a * c;
     if discriminant < 0.0 {
-        return vec![];
+        return None;
     }
 
     let a_x1 = -0.5 * (b + f32::copysign(discriminant.sqrt(), b));
 
-    vec![a_x1 / a, c / a_x1]
+    Some((a_x1 / a, c / a_x1))
 }
