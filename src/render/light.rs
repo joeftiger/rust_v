c49c8c0a5cc762449373f33c3767ba438a39aaff
Julius Oeftiger <j.oeftiger@protonmail.com>
2020-10-27 20:14:28 +0100

    Simplify Light to a point light for now

--- src/render/light.rs
+++ src/render/light.rs
@@ -1,26 +1,19 @@
 use ultraviolet::Vec3;
 
 use crate::color::Srgb;
-use crate::geometry::aabb::Aabb;
 use crate::geometry::ray::Ray;
-use crate::geometry::{Geometry, GeometryInfo, Hit};
-use std::ops::Mul;
-use crate::render::scene_objects::SceneObject;
+use crate::floats::BIG_EPSILON;
 
 pub struct Light {
     pub point: Vec3,
-    pub object: SceneObject,
     pub color: Srgb,
-    pub intensity: f32,
 }
 
 impl Light {
-    pub fn new(point: Vec3, object: SceneObject, color: Srgb, intensity: f32) -> Self {
+    pub fn new(point: Vec3, color: Srgb) -> Self {
         Self {
             point,
-            object,
             color,
-            intensity,
         }
     }
 
@@ -33,36 +26,10 @@ impl Light {
     }
 
     pub fn ray_to(&self, point: Vec3) -> Ray {
-        Ray::new_simple(self.point, self.direction_to(point))
+        Ray::new(self.point, self.direction_to(point), self.distance(point) + BIG_EPSILON)
     }
 
     pub fn distance(&self, point: Vec3) -> f32 {
         (self.point - point).mag()
-    }
-
-    pub fn intensity_at(&self, point: Vec3) -> f32 {
-        let distance = self.distance(point);
-
-        self.intensity / (distance * distance)
-    }
-
-    pub fn color_at(&self, point: Vec3) -> Srgb {
-        let intensity = self.intensity_at(point);
-
-        self.color.mul(intensity)
-    }
-}
-
-impl Geometry for Light {
-    fn bounding_box(&self) -> Aabb {
-        self.object.bounding_box()
-    }
-
-    fn intersect(&self, ray: &Ray) -> Option<f32> {
-        self.object.intersect(ray)
-    }
-
-    fn get_info(&self, hit: Hit) -> GeometryInfo {
-        self.object.get_info(hit)
     }
 }
