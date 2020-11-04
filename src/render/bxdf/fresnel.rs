e37c7c6c2f329f75e29db92c37c49a345839fc63
Julius Oeftiger <j.oeftiger@protonmail.com>
2020-11-04 21:21:43 +0100

    Put Fresnel reflection into correct file

--- src/render/bxdf/fresnel.rs
+++ src/render/bxdf/fresnel.rs
@@ -1,6 +1,82 @@
 use crate::color::Color;
-use crate::render::bxdf::reflection;
 use crate::Spectrum;
+
+use crate::floats;
+use bitflags::_core::mem::swap;
+
+#[inline(always)]
+#[must_use]
+pub fn dielectric_parallel(cos_i: f32, cos_t: f32, eta_i: f32, eta_t: f32) -> f32 {
+    let it = eta_i * cos_t;
+    let ti = eta_t * cos_i;
+
+    (ti - it) / (ti + it)
+}
+
+#[inline(always)]
+#[must_use]
+pub fn dielectric_perpendicular(cos_i: f32, cos_t: f32, eta_i: f32, eta_t: f32) -> f32 {
+    let tt = eta_t * cos_t;
+    let ii = eta_i * cos_i;
+
+    (ii - tt) / (ii + tt)
+}
+
+#[must_use]
+pub fn fresnel_dielectric(mut cos_i: f32, mut eta_i: f32, mut eta_t: f32) -> f32 {
+    cos_i = floats::fast_clamp(cos_i, -1.0, 1.0);
+    // potentially swap indices of refraction
+    let entering = cos_i > 0.0;
+    if entering {
+        swap(&mut eta_i, &mut eta_t);
+        cos_i = cos_i.abs();
+    }
+
+    // compute cos_t using Snell's law
+    let sin_i = floats::fast_max(0.0, 1.0 - cos_i * cos_i).sqrt();
+    let sin_t = eta_i * sin_i / eta_t;
+
+    // handle total internal reflection
+    if sin_t >= 1.0 {
+        return 1.0;
+    }
+
+    let cos_t = floats::fast_max(0.0, 1.0 - sin_t * sin_t).sqrt();
+    let r_par = dielectric_parallel(cos_i, cos_t, eta_i, eta_t);
+    let r_perp = dielectric_perpendicular(cos_i, cos_t, eta_i, eta_t);
+
+    (r_par * r_par + r_perp * r_perp) / 2.0
+}
+
+#[must_use]
+pub fn fresnel_conductor(
+    mut cos_i: f32,
+    eta_i: &Spectrum,
+    eta_t: &Spectrum,
+    k: &Spectrum,
+) -> Spectrum {
+    cos_i = floats::fast_clamp(cos_i, -1.0, 1.0);
+    let eta = *eta_t / *eta_i;
+    let etak = *k / *eta_i;
+
+    let cos_i2 = cos_i * cos_i;
+    let sin_i2 = 1.0 - cos_i2;
+    let eta2 = eta * eta;
+    let etak2 = etak * etak;
+
+    let t0 = eta2 - etak2 - sin_i2.into();
+    let a2_plus_b2 = (t0 * t0 + eta2 * etak2 * 4.0).sqrt();
+    let t1 = a2_plus_b2 + cos_i2.into();
+    let a = ((a2_plus_b2 + t0) * 0.5).sqrt();
+    let t2 = a * (cos_i * 2.0);
+    let r_s = (t1 - t2) / (t1 + t2);
+
+    let t3 = a2_plus_b2 * cos_i2 + (sin_i2 * sin_i2).into();
+    let t4 = t2 * sin_i2;
+    let r_p = r_s * ((t3 - t4) / (t3 + t4));
+
+    (r_p + r_s) / 2.0
+}
 
 pub trait Fresnel: Send + Sync {
     fn evaluate(&self, cos_i: f32) -> Spectrum;
@@ -21,7 +97,7 @@ impl Dielectric {
 
 impl Fresnel for Dielectric {
     fn evaluate(&self, cos_i: f32) -> Spectrum {
-        reflection::fresnel_dielectric(cos_i, self.eta_i, self.eta_t).into()
+        fresnel_dielectric(cos_i, self.eta_i, self.eta_t).into()
     }
 }
 
@@ -45,7 +121,7 @@ impl Conductor {
 
 impl Fresnel for Conductor {
     fn evaluate(&self, cos_i: f32) -> Spectrum {
-        reflection::fresnel_conductor(cos_i.abs(), &self.eta_i, &self.eta_t, &self.k)
+        fresnel_conductor(cos_i.abs(), &self.eta_i, &self.eta_t, &self.k)
     }
 }
 
