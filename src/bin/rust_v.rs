04c43a9517b0379502773c46f9ebb9d6e13ab86d
Julius Oeftiger <j.oeftiger@protonmail.com>
2020-11-09 23:53:05 +0100

    Cleanup bin/rust_v.rs to use feature 'live-window'

--- src/bin/rust_v.rs
+++ src/bin/rust_v.rs
@@ -1,92 +1,72 @@
-use clap::{App, Arg, SubCommand};
+#[macro_use]
+extern crate clap;
 
-use rust_v::cornell_box;
-use rust_v::render::camera::Camera;
-use rust_v::render::renderer::debug::NormalRenderer;
+use clap::{App, ArgMatches};
+
 use rust_v::render::renderer::{Renderer, RgbRenderer};
-use rust_v::render::scene::Scene;
+use rust_v::render::renderer::debug::NormalRenderer;
 use rust_v::render::window::RenderWindow;
+use rust_v::cornell_box;
 
-const LIVE_WINDOW: &str = "LIVE_WINDOW";
+const LIVE_WINDOW: &str = "live_window";
 const DEMO: &str = "demo";
-const DEBUG_RENDERER: &str = "DEBUG_RENDERER";
-const INPUT: &str = "IN";
-const OUTPUT: &str = "OUT";
+const DEBUG_RENDERER: &str = "DEBUG";
+#[allow(dead_code)]
+const INPUT: &str = "INPUT";
+const OUTPUT: &str = "OUTPUT";
 
 fn main() {
-    let mut app = init_help();
-    let matches = app.clone().get_matches();
+    #[cfg(not(feature = "live-window"))]
+        let yaml = load_yaml!("cli.yml");
+    #[cfg(feature = "live-window")]
+        let yaml = load_yaml!("cli-live-window.yml");
+
+    let matches = App::from_yaml(yaml).get_matches();
 
     if let Some(demo) = matches.subcommand_matches(DEMO) {
-        let (scene, camera) = create_cornell_box();
+        let (scene, camera) = cornell_box::create(900, 900);
 
-        let mut renderer: Box<dyn Renderer>;
-        if demo.is_present(DEBUG_RENDERER) {
-            renderer = Box::new(NormalRenderer::new(scene, camera));
-        } else {
-            renderer = Box::new(RgbRenderer::new(scene, camera));
-        }
+        let renderer: Box<dyn Renderer> = {
+            if demo.is_present(DEBUG_RENDERER) {
+                Box::new(NormalRenderer::new(scene, camera))
+            } else {
+                Box::new(RgbRenderer::new(scene, camera))
+            }
+        };
 
-        if demo.is_present(LIVE_WINDOW) {
-            let mut window =
-                RenderWindow::new("Rust-V".to_string(), renderer).expect("Can't create window");
-
-            window.start_rendering();
-        } else {
-            renderer.render_all();
-
-            let image = renderer.get_image_u16();
-            image.save("./rendering.png").unwrap();
-        }
-    } else {
-        app.print_help().expect("Could not print help message");
+        render(demo, renderer);
     }
 }
 
-fn init_help<'a, 'b>() -> App<'a, 'b> {
-    let app = App::new("Rust-V")
-        .version("0.0.1")
-        .author("Julius Oeftiger")
-        .about("A rust ray tracer supporting rgb and spectral ray tracing")
-        .arg(
-            Arg::with_name(LIVE_WINDOW)
-                .short("l")
-                .long("live")
-                .help("Use a live window to show rendering progressively. The window allows saving with [Ctrl + S]")
-                .required(false)
-        )
-        .arg(
-            Arg::with_name(INPUT)
-                .required(false)
-        )
-        .arg(
-            Arg::with_name(OUTPUT)
-                .required(false)
-        );
-
-    let demo = SubCommand::with_name(DEMO)
-        .version("0.0.1")
-        .author("Julius Oeftiger")
-        .about("The cornell box demo scene")
-        .arg(
-            Arg::with_name(DEBUG_RENDERER)
-                .long("--debug")
-                .help("Use a debugging renderer to render surface normals.")
-                .required(false),
-        )
-        .arg(
-            Arg::with_name(LIVE_WINDOW)
-                .long("live")
-                .help("Use a live window to show rendering progressively. The window allows saving with [Ctrl + S]")
-                .required(false)
-        );
-
-    app.subcommand(demo)
+#[cfg(not(feature = "live-window"))]
+fn render(matches: &ArgMatches, mut renderer: Box<dyn Renderer>) {
+    render_and_save(matches, renderer);
 }
 
-fn create_cornell_box() -> (Scene, Camera) {
-    (
-        cornell_box::create_box(),
-        cornell_box::create_camera(900, 900),
-    )
+#[cfg(feature = "live-window")]
+fn render(matches: &ArgMatches, renderer: Box<dyn Renderer>) {
+    if matches.is_present(LIVE_WINDOW) {
+        RenderWindow::new("Rust-V".to_string(), renderer)
+                .expect("Can't create window")
+                .start_rendering();
+    } else {
+        render_and_save(matches, renderer);
+    }
+}
+
+fn render_and_save(matches: &ArgMatches, mut renderer: Box<dyn Renderer>) {
+    renderer.render_all();
+    let image = renderer.get_image_u16();
+
+    if let Some(output) = matches.value_of_os(OUTPUT) {
+        image.save(&output)
+            .unwrap_or_else(|_| panic!("Could not save to: {:?}", output));
+    } else {
+        let start = std::time::SystemTime::now();
+        let since = start.duration_since(std::time::UNIX_EPOCH).expect("Time went backwards");
+        let file = format!("{}.png", since.as_nanos());
+
+        image.save(&file)
+            .unwrap_or_else(|_| panic!("Could not save to: {}", file));
+    }
 }
