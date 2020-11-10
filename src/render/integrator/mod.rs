f05aae5b6e5a542635f4949bcdc80ebbb2b198f4
Julius Oeftiger <j.oeftiger@protonmail.com>
2020-11-10 21:51:53 +0100

    Work on integrators

--- src/render/integrator/mod.rs
+++ src/render/integrator/mod.rs
@@ -1,11 +1,23 @@
+pub mod debug_normals;
+
+use crate::render::sampler::Sampler;
 use crate::render::scene::{Scene, SceneIntersection};
 use crate::Spectrum;
 
 pub trait Integrator {
     fn illumination(&self, scene: &Scene, intersection: &SceneIntersection) -> Spectrum;
 
-    fn specular_reflection(&self, _scene: &Scene, intersection: &SceneIntersection) -> Spectrum {
-        let _outgoing = -intersection.info.ray.direction;
+    #[allow(unused_variables)]
+    fn specular_reflection(
+        &self,
+        scene: &Scene,
+        intersection: &SceneIntersection,
+        sampler: &mut dyn Sampler,
+    ) -> Spectrum {
+        let outgoing = -intersection.info.ray.direction;
+
+        let sample = sampler.get_2d();
+        let bsdf = &intersection.obj.bxdf;
 
         unimplemented!()
     }
