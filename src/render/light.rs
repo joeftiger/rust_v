3adbb536d6d74bec096cf02cddde231487b25a1d
Julius Oeftiger <j.oeftiger@protonmail.com>
2020-11-10 22:53:13 +0100

    Use arguments as Vec3 references

--- src/render/light.rs
+++ src/render/light.rs
@@ -18,29 +18,29 @@ impl Light {
         }
     }
 
-    pub fn direction_from(&self, point: Vec3) -> Vec3 {
-        (self.point - point).normalized()
+    pub fn direction_from(&self, point: &Vec3) -> Vec3 {
+        (self.point - *point).normalized()
     }
 
-    pub fn direction_to(&self, point: Vec3) -> Vec3 {
-        (point - self.point).normalized()
+    pub fn direction_to(&self, point: &Vec3) -> Vec3 {
+        (*point - self.point).normalized()
     }
 
-    pub fn ray_to(&self, point: Vec3) -> Ray {
-        let diff = point - self.point;
+    pub fn ray_to(&self, point: &Vec3) -> Ray {
+        let diff = *point - self.point;
         Ray::new(self.point, diff.normalized(), diff.mag())
     }
 
-    pub fn ray_from(&self, point: Vec3) -> Ray {
-        let diff = self.point - point;
+    pub fn ray_from(&self, point: &Vec3) -> Ray {
+        let diff = self.point - *point;
         Ray::new(self.point, diff.normalized(), diff.mag())
     }
 
-    pub fn distance(&self, point: Vec3) -> f32 {
-        (self.point - point).mag()
+    pub fn distance(&self, point: &Vec3) -> f32 {
+        (self.point - *point).mag()
     }
 
-    pub fn intensity_at(&self, point: Vec3) -> Spectrum {
+    pub fn intensity_at(&self, point: &Vec3) -> Spectrum {
         let dist = self.distance(point);
 
         self.color * (self.intensity / (dist * dist))
