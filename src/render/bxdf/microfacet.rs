4cca698f79b9ce5e832c3bdc613ef0d12194cbb4
Julius Oeftiger <j.oeftiger@protonmail.com>
2020-11-20 14:40:40 +0100

    MicrofacetTransmission

--- src/render/bxdf/microfacet.rs
+++ src/render/bxdf/microfacet.rs
@@ -1,11 +1,14 @@
-use crate::Spectrum;
-use ultraviolet::{Vec3, Vec2};
-use util::{floats, math};
-use crate::render::bxdf::*;
-use std::f32::consts::{PI, FRAC_PI_2};
-use geometry::spherical_direction;
-use crate::render::bxdf::fresnel::Fresnel;
+use std::f32::consts::{FRAC_PI_2, PI};
+
+use ultraviolet::{Vec2, Vec3};
+
 use color::Color;
+use geometry::spherical_direction;
+use util::{floats, math};
+
+use crate::render::bxdf::fresnel::{Dielectric, Fresnel};
+use crate::render::bxdf::*;
+use crate::Spectrum;
 
 #[allow(dead_code)]
 pub fn roughness_to_alpha(roughness: f32) -> f32 {
@@ -46,35 +49,37 @@ pub trait MicrofacetDistribution: Debug {
 pub struct BeckmannDistribution {
     alpha_x: f32,
     alpha_y: f32,
-    sample_visible_area: bool
+    sample_visible_area: bool,
 }
 
 impl BeckmannDistribution {
     pub fn new(alpha_x: f32, alpha_y: f32, sample_visible_area: bool) -> Self {
-        Self { alpha_x, alpha_y, sample_visible_area }
+        Self {
+            alpha_x,
+            alpha_y,
+            sample_visible_area,
+        }
     }
 
     fn beckmann_sample11(cos_theta_i: f32, sample: &Vec2) -> Vec2 {
         /* Special case (normal incidence) */
         if cos_theta_i > 1.0 - floats::BIG_EPSILON {
-            let r = f32::sqrt(- f32::ln(1.0 - sample.x));
+            let r = f32::sqrt(-f32::ln(1.0 - sample.x));
             let phi = 2.0 * PI * sample.y;
 
-            return Vec2::new(
-                r * phi.sin(),
-                r * phi.cos());
+            return Vec2::new(r * phi.sin(), r * phi.cos());
         }
 
         /* The original inversion routine from the paper contained
-           discontinuities, which causes issues for QMC integration
-           and techniques like Kelemen-style MLT. The following code
-           performs a numerical inversion with better behavior */
+        discontinuities, which causes issues for QMC integration
+        and techniques like Kelemen-style MLT. The following code
+        performs a numerical inversion with better behavior */
         let sin_theta_i = 0f32.max(1.0 - cos_theta_i * cos_theta_i).sqrt();
         let tan_theta_i = sin_theta_i / cos_theta_i;
         let cot_theta_i = 1.0 / tan_theta_i;
 
         /* Search interval -- everything is parameterized
-           in the Erf() domain */
+        in the Erf() domain */
         let mut a = -1.0;
         let mut c = math::erf(cot_theta_i);
         let sample_x = sample.x.max(floats::BIG_EPSILON);
@@ -90,21 +95,26 @@ impl BeckmannDistribution {
 
         /* Normalization factor for the CDF */
 
-        let normalization = 1.0 / (1.0 + c + floats::FRAC_1_SQRT_PI * tan_theta_i * f32::exp(-cot_theta_i * cot_theta_i));
+        let normalization = 1.0
+            / (1.0
+                + c
+                + floats::FRAC_1_SQRT_PI * tan_theta_i * f32::exp(-cot_theta_i * cot_theta_i));
 
         for _ in 0..10 {
             /* Bisection criterion -- the oddly-looking
-               Boolean expression are intentional to check
-               for NaNs at little additional cost */
+            Boolean expression are intentional to check
+            for NaNs at little additional cost */
             // if !(b >= a && b <= c) {
             if b < a || b > c {
                 b = 0.5 * (a + c);
             }
 
             /* Evaluate the CDF and its derivative
-               (i.e. the density function) */
+            (i.e. the density function) */
             let inv_erf = math::erf_inv(b);
-            let value = normalization * (1.0 + b + floats::FRAC_1_SQRT_PI * tan_theta_i * f32::exp(-inv_erf * inv_erf)) - sample_x;
+            let value = normalization
+                * (1.0 + b + floats::FRAC_1_SQRT_PI * tan_theta_i * f32::exp(-inv_erf * inv_erf))
+                - sample_x;
             if value.abs() < floats::BIG_EPSILON {
                 break;
             }
@@ -232,7 +242,9 @@ impl MicrofacetDistribution for BeckmannDistribution {
             let sin_phi = phi.sin();
             let cos_phi = phi.cos();
 
-            tan2_theta = -log_sample / (cos_phi * cos_phi / alpha_x2 + sin_phi * sin_phi / (self.alpha_y * self.alpha_y));
+            tan2_theta = -log_sample
+                / (cos_phi * cos_phi / alpha_x2
+                    + sin_phi * sin_phi / (self.alpha_y * self.alpha_y));
         }
 
         // Map sampled Beckmann angles to normal direction _wh_
@@ -302,8 +314,16 @@ pub struct MicrofacetReflection {
 }
 
 impl MicrofacetReflection {
-    pub fn new(r: Spectrum, distribution: Box<dyn MicrofacetDistribution>, fresnel: Box<dyn Fresnel>) -> Self {
-        Self { r, distribution, fresnel }
+    pub fn new(
+        r: Spectrum,
+        distribution: Box<dyn MicrofacetDistribution>,
+        fresnel: Box<dyn Fresnel>,
+    ) -> Self {
+        Self {
+            r,
+            distribution,
+            fresnel,
+        }
     }
 }
 
@@ -330,5 +350,73 @@ impl BxDF for MicrofacetReflection {
         let dist = self.r * self.distribution.d(&wh) * self.distribution.g(incident, outgoing);
 
         dist * f / (4.0 * cos_theta_i * cos_theta_o)
+    }
+}
+
+#[derive(Debug)]
+pub struct MicrofacetTransmission {
+    t: Spectrum,
+    distribution: Box<dyn MicrofacetDistribution>,
+    fresnel: Dielectric,
+}
+
+impl MicrofacetTransmission {
+    pub fn new(
+        t: Spectrum,
+        distribution: Box<dyn MicrofacetDistribution>,
+        fresnel: Dielectric,
+    ) -> Self {
+        Self {
+            t,
+            distribution,
+            fresnel,
+        }
+    }
+}
+
+impl BxDF for MicrofacetTransmission {
+    fn get_type(&self) -> BxDFType {
+        BxDFType::TRANSMISSION | BxDFType::GLOSSY
+    }
+
+    fn evaluate(&self, incident: &Vec3, outgoing: &Vec3) -> Spectrum {
+        if same_hemisphere(incident, outgoing) {
+            return Spectrum::black();
+        }
+
+        let cos_theta_i = cos_theta(incident);
+        let cos_theta_o = cos_theta(outgoing);
+        if cos_theta_i == 0.0 || cos_theta_o == 0.0 {
+            return Spectrum::black();
+        }
+
+        // Compute $\wh$ from $\wo$ and $\wi$ for microfacet transmission
+        let (eta, wh) = {
+            let eta = if cos_theta_o > 0.0 {
+                self.fresnel.eta_t / self.fresnel.eta_i
+            } else {
+                self.fresnel.eta_i / self.fresnel.eta_t
+            };
+            let wh = flip_if_neg(*outgoing + *incident * eta);
+            (eta, wh)
+        };
+
+        // Same side?
+        let cos_i = outgoing.dot(wh);
+        let cos_t = incident.dot(wh);
+        if cos_i * cos_t > 0.0 {
+            return Spectrum::black();
+        }
+
+        let f = self.fresnel.evaluate(cos_i);
+
+        let sqrt_denom = cos_i + eta * cos_t;
+
+        let t = (Spectrum::new_const(1.0) - f) * self.t;
+        let dist = self.distribution.d(&wh) * self.distribution.g(incident, outgoing);
+        let factor =
+            cos_i.abs() * cos_t.abs() / (cos_theta_i * cos_theta_i * sqrt_denom * sqrt_denom);
+
+        t * (dist * factor).abs()
     }
 }
