3bd0d8b5c3101ca2d54063c16e2d184061d6e442
Julius Oeftiger <j.oeftiger@protonmail.com>
2020-11-23 14:17:05 +0100

    Better config

--- src/bin/rust_v.rs
+++ src/bin/rust_v.rs
@@ -13,11 +13,11 @@ use rust_v::render::sampler::RandomSampler;
 use rust_v::render::window::RenderWindow;
 use std::convert::TryInto;
 use std::sync::{Arc, Mutex};
+use rust_v::render::integrator::path::Path;
 
 const LIVE: &str = "LIVE_WINDOW";
 const DEMO: &str = "demo";
 const VERBOSE: &str = "VERBOSE";
-const DEBUG: &str = "DEBUG";
 #[allow(dead_code)]
 const INPUT: &str = "INPUT";
 const OUTPUT: &str = "OUTPUT";
@@ -26,6 +26,7 @@ const DEPTH: &str = "DEPTH";
 const WIDTH: &str = "WIDTH";
 const HEIGHT: &str = "HEIGHT";
 const FORMAT: &str = "FORMAT";
+const INTEGRATOR_BACKEND: &str = "INTEGRATOR_BACKEND";
 
 fn main() -> Result<(), String> {
     let yaml = load_yaml!("cli.yml");
@@ -34,7 +35,6 @@ fn main() -> Result<(), String> {
 
     if let Some(demo) = matches.subcommand_matches(DEMO) {
         let verbose = demo.is_present(VERBOSE);
-        let debug = demo.is_present(DEBUG);
         let width = match demo.value_of(WIDTH).unwrap_or("900").parse() {
             Ok(width) => width,
             Err(err) => panic!("Cannot parse width: {}", err),
@@ -52,9 +52,13 @@ fn main() -> Result<(), String> {
             Err(err) => panic!("Cannot parse passes: {}", err),
         };
         let live = demo.is_present(LIVE);
-        let pixel_format = match demo.value_of(FORMAT).unwrap_or("u8").try_into() {
+        let pixel_format = match demo.value_of(FORMAT).unwrap_or("U8").try_into() {
             Ok(format) => format,
             Err(err) => panic!("Cannot parse pixel format: {}", err),
+        };
+        let integrator_backend = match demo.value_of(INTEGRATOR_BACKEND).unwrap_or("whitted").try_into() {
+            Ok(integrator) => integrator,
+            Err(err) => panic!("Cannot parse integrator backend: {}", err),
         };
 
         let output = if let Some(o) = demo.value_of(OUTPUT) {
@@ -70,9 +74,8 @@ fn main() -> Result<(), String> {
         };
         let output = output.as_str();
 
-        let mut main = Main::new(
+        let mut main = Configuration::new(
             verbose,
-            debug,
             width,
             height,
             depth,
@@ -80,6 +83,7 @@ fn main() -> Result<(), String> {
             live,
             output,
             pixel_format,
+            integrator_backend
         );
         main.start()
     } else {
@@ -88,9 +92,8 @@ fn main() -> Result<(), String> {
 }
 
 #[derive(Debug)]
-struct Main<'a> {
+struct Configuration<'a> {
     verbose: bool,
-    debug: bool,
     width: u32,
     height: u32,
     depth: u32,
@@ -98,13 +101,13 @@ struct Main<'a> {
     live: bool,
     output: &'a str,
     pixel_format: PixelFormat,
+    integrator_backend: IntegratorBackend,
 }
 
-impl<'a> Main<'a> {
+impl<'a> Configuration<'a> {
     #[allow(clippy::too_many_arguments)]
     fn new(
         verbose: bool,
-        debug: bool,
         width: u32,
         height: u32,
         depth: u32,
@@ -112,10 +115,10 @@ impl<'a> Main<'a> {
         live: bool,
         output: &'a str,
         pixel_format: PixelFormat,
+        integrator_backend: IntegratorBackend,
     ) -> Self {
         Self {
             verbose,
-            debug,
             width,
             height,
             depth,
@@ -123,6 +126,7 @@ impl<'a> Main<'a> {
             live,
             output,
             pixel_format,
+            integrator_backend,
         }
     }
 
@@ -133,11 +137,13 @@ impl<'a> Main<'a> {
 
         let (scene, camera) = cornell_box::create(self.width, self.height);
         let sampler = Mutex::new(RandomSampler);
-        let integrator: Arc<dyn Integrator> = if self.debug {
-            Arc::new(DebugNormals)
-        } else {
-            Arc::new(Whitted::new(self.depth))
+
+        let integrator: Arc<dyn Integrator> = match self.integrator_backend {
+            IntegratorBackend::Debug => Arc::new(DebugNormals),
+            IntegratorBackend::Whitted => Arc::new(Whitted::new(self.depth)),
+            IntegratorBackend::Path => Arc::new(Path::new(0, self.depth)),
         };
+
         let mut renderer = Renderer::new(
             Arc::new(scene),
             Arc::new(camera),
@@ -165,11 +171,11 @@ impl<'a> Main<'a> {
             bar.lock().expect("ProgressBar poisoned").finish();
 
             match self.pixel_format {
-                PixelFormat::u8 => renderer
+                PixelFormat::U8 => renderer
                     .get_image_u8()
                     .save(&self.output)
                     .map_err(|e| format!("Unable to save image: {}", e))?,
-                PixelFormat::u16 => renderer
+                PixelFormat::U16 => renderer
                     .get_image_u16()
                     .save(&self.output)
                     .map_err(|e| format!("Unable to save image: {}", e))?,
@@ -180,11 +186,10 @@ impl<'a> Main<'a> {
     }
 }
 
-#[allow(non_camel_case_types)]
 #[derive(Debug)]
 enum PixelFormat {
-    u8,
-    u16,
+    U8,
+    U16,
 }
 
 impl TryInto<PixelFormat> for &str {
@@ -192,8 +197,26 @@ impl TryInto<PixelFormat> for &str {
 
     fn try_into(self) -> Result<PixelFormat, Self::Error> {
         match self {
-            "u8" => Ok(PixelFormat::u8),
-            "u16" => Ok(PixelFormat::u16),
+            "u8" | "U8" => Ok(PixelFormat::U8),
+            "u16" | "U16" => Ok(PixelFormat::U16),
+            _ => Err(self.to_string()),
+        }
+    }
+}
+
+#[derive(Debug)]
+enum IntegratorBackend {
+    Debug, Whitted, Path,
+}
+
+impl TryInto<IntegratorBackend> for &str {
+    type Error = String;
+
+    fn try_into(self) -> Result<IntegratorBackend, Self::Error> {
+        match self {
+            "debug" | "Debug" | "DEBUG" => Ok(IntegratorBackend::Debug),
+            "whitted" | "Whitted" | "WHITTED" => Ok(IntegratorBackend::Whitted),
+            "path" | "Path" | "PATH" => Ok(IntegratorBackend::Path),
             _ => Err(self.to_string()),
         }
     }
