a9dfe631af1d12c9e6584a2b2d4229cbc822647b
Julius Oeftiger <j.oeftiger@protonmail.com>
2020-11-26 13:58:34 +0100

    Fix PGO

--- pgo.sh
+++ pgo.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-export RUSTFLAGS="-Ctarget-cpu=native -Cprofile-generate=./pgo-data"
+export RUSTFLAGS="-Ctarget-cpu=native -Cprofile-generate=/tmp/pgo-data"
 
-cargo run --package rust_v --bin rust_v --release -- demo -w 400 -h 400 -d 3 -p 1 -i path --block-size 8 --threaded
-cargo run --package rust_v --bin rust_v --release -- demo -w 300 -h 300 -d 6 -p 2 -i path --block-size 8 --threaded
-cargo run --package rust_v --bin rust_v --release -- demo -w 200 -h 200 -d 10 -p 4 -i path --block-size 8 --threaded
-cargo run --package rust_v --bin rust_v --release -- demo -w 500 -h 500 -d 10 -p 8 -i path --block-size 8 --threaded
+cargo run --package rust_v --bin rust_v --release -- demo -w 400 -h 400 -d 3 -p 1 -i path --block-size 8 --threaded -o ""
+cargo run --package rust_v --bin rust_v --release -- demo -w 300 -h 300 -d 6 -p 2 -i path --block-size 8 --threaded -o ""
+cargo run --package rust_v --bin rust_v --release -- demo -w 200 -h 200 -d 10 -p 4 -i path --block-size 8 --threaded -o ""
+cargo run --package rust_v --bin rust_v --release -- demo -w 500 -h 500 -d 10 -p 8 -i path --block-size 8 --threaded -o ""
 
-$HOME/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/x86_64-unknown-linux-gnu/bin/llvm-profdata merge -o ./pgo-data.profdata ./pgo-data
+$HOME/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/x86_64-unknown-linux-gnu/bin/llvm-profdata merge -o /tmp/pgo-data.profdata /tmp/pgo-data
 
-export RUSTFLAGS="-Ctarget-cpu=native -Cprofile-use=./pgo-data.profdata"
+export RUSTFLAGS="-Ctarget-cpu=native -Cprofile-use=/tmp/pgo-data.profdata"
 cargo build --package rust_v --bin rust_v --release
